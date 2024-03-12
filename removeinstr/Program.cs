6035fa69a1462214619738b844c07d460e855730
Arsen <Cr1zzzzz@users.noreply.github.com>
2024-03-12 13:49:44 +0200

    Add project files.

---
+++ removeinstr/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Введення рядків
+        Console.WriteLine("Введіть перший рядок:");
+        string mainString = Console.ReadLine();
+
+        Console.WriteLine("Введіть другий рядок:");
+        string substring = Console.ReadLine();
+        int index;
+        while ((index = mainString.IndexOf(substring)) != -1)
+        {
+            mainString = mainString.Remove(index, substring.Length);
+        }
+        // Виведення результату
+        Console.WriteLine("Результат:");
+        Console.WriteLine(mainString);
+    }
+}
